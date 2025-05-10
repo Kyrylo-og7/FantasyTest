f70a2d28f0501fc4fac5564f809401661e0f544b
kirill <homaksk84@gmail.com>
2025-05-10 17:56:22 +0200

    Initial commit

---
+++ js/app1.js
@@ -0,0 +1,10 @@
+VANTA.TOPOLOGY({
+    el: "#intro1",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 2900.00,
+    minWidth: 1080.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
